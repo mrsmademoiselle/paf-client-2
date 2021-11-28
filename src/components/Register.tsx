a1c9966aa9a07513702490ac3307fac735481d03
Alexander Wiederspan <alexander.wiederspan@stud.th-luebeck.de>
2021-11-28 03:52:44 +0100

    #6 #7 adjust protokoll in fetch for windows devices (snowflakes...), remove bloated modal window code, add banners STILL CORS IF SPRING SERVER DOES NOT IMPLEMENT IT!!!!!!111 React client does not behave the same way as FX Client since preflight mechanism of underlaying browser see issue for more info

--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -1,5 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import React, {useState} from 'react';
+import React, { Component, useEffect, useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Container from 'react-bootstrap/Container';
@@ -9,7 +9,8 @@ import placeHolderImg from '../images/100.jpg'
 import editImg from '../images/edit.svg'
 import '../App.css';
 import TopNavigationBar from './TopNavigationBar'
-import {Modal} from 'react-bootstrap';
+import Alert from 'react-bootstrap/Alert';
+import { Modal } from 'react-bootstrap';
 
 function fileUpload() {
     // not yet implemented
@@ -29,83 +30,54 @@ function setCurrentPic() {
     // not yet implemented
 }
 
-async function handleSubmit() {
-    /**
-     * Get information from the form and write into json
-     * then fetch the endpoint with the json
-     * Handle if server response is not 200 or if anything else, e.g. network error, occurs
-     */
-    var userName = (document.getElementById("userName")! as HTMLInputElement).value!;
-    var userPW = (document.getElementById("userPassword")! as HTMLInputElement).value!;
+function Register() {
+    const [banner, setBanner] = React.useState<boolean | undefined>();
+    
+    const onSubmit = (e: { preventDefault: () => void; }) => {
+        e.preventDefault();
+        handleSubmit();
+    };
 
-    // Attach Pitures to body
-    var userPic = (document.getElementById("userPic")! as HTMLInputElement).value!;
-    const formData = new FormData();
-    formData.append('userImage', userPic);
+    async function handleSubmit() {
+        /**
+         * Get information from the form and write into json
+         * then fetch the endpoint with the json
+         * Handle if server response is not 200 or if anything else, e.g. network error, occurs
+         * Create Banner based on server response or if an exception happens
+         */
+        var userName = (document.getElementById("userName")! as HTMLInputElement).value!;
+        var userPW = (document.getElementById("userPassword")! as HTMLInputElement).value!;
 
-    /* Body mit Bildern
         const requestBody = {
             username: userName,
             userpassword: userPW,
-            userPic: userPic,
-            formData
-        }*/
+        }
+        try {
+            await fetch('http://localhost:9090/user/register/', {
+                method: 'POST',
+                body: JSON.stringify(requestBody),
+                headers: { 'Content-Type': 'application/json' },
+                mode: 'cors',
+            })
+                .then(serverResponse => {
+                    if (!serverResponse.ok) {
+                        return (setBanner(false))
+                    } else {
+                        return (setBanner(true))
+                    }
 
-    const requestBody = {
-        username: userName,
-        userpassword: userPW,
+                });
+        } catch (exception) {
+            return (setBanner(false))
+        }
     }
-    try {
-        await fetch('localhost:9090/user/register', {
-            method: 'POST',
-            body: JSON.stringify(requestBody),
-            headers: {'Content-Type': 'application/json'}
-        })
-            .then(serverResponse => {
-                if (!serverResponse.ok) {
-                    alert("Es gab ein Problem mit dem Server");
-                }
-            });
-    } catch (exception) {
-        // Modalwindow - can be changed to banner if needed
-        //  return(<ModalWindow/>)
-        alert("Es gab ein Problem beim registrieren! " + exception);
-    }
-}
-
-function ModalWindow() {
-    // TODO: Modalfenster wenn gewuenscht vgl. React Bootstrap Doku
-    const [show, setShow] = useState(false);
-
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
-
-    return (
-        <>
-            <Modal show={show} onHide={handleClose}>
-                <Modal.Header closeButton>
-                    <Modal.Title>Modal heading</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-                <Modal.Footer>
-                    <Button variant="secondary" onClick={handleClose}>
-                        Close
-                    </Button>
-                    <Button variant="primary" onClick={handleClose}>
-                        Save Changes
-                    </Button>
-                </Modal.Footer>
-            </Modal>
-        </>
-    );
-}
-
-
-function Register() {
 
     return (
         <div className="App">
-            <TopNavigationBar/>
+            <TopNavigationBar />
+            {/* Setzten des Banners */}
+            {typeof banner == "undefined" ? null :
+                banner ? <Alert variant="success">Registrieren erfolgreich!</Alert> : <Alert variant="danger">Registrieren fehlgeschlagen!</Alert>}
             <Container>
                 <Row>
                     <Col>
@@ -120,29 +92,29 @@ function Register() {
                                 {/* Profilbild */}
                                 <Row className="d-flex justify-content-center middleRow">
                                     <img alt="Standard Anzeigebild" className="col-auto profilePic"
-                                         onClick={fileUpload} src={placeHolderImg} title="Bild hochladen"
-                                         onMouseEnter={hoverEffect} onMouseLeave={setCurrentPic}/>
+                                        onClick={fileUpload} src={placeHolderImg} title="Bild hochladen"
+                                        onMouseEnter={hoverEffect} onMouseLeave={setCurrentPic} />
 
                                     <Button className="col-auto uploadButton">
                                         <img className="uploadButtonIcon"
-                                             src={editImg} title="Bild hochladen"/>
+                                            src={editImg} title="Bild hochladen" />
                                     </Button>
                                 </Row>
 
                                 {/* User Input */}
                                 <Row>
                                     <Col>
-                                        <Form className="registerForm" onSubmit={handleSubmit}>
+                                        <Form className="registerForm" onSubmit={onSubmit}>
                                             <Form.Group className="mb-3" controlId="userName">
-                                                <Form.Control type="text" placeholder="Benutzername" required/>
+                                                <Form.Control type="text" placeholder="Benutzername" required />
                                             </Form.Group>
                                             <Form.Group className="mb-3" controlId="userPassword">
-                                                <Form.Control type="password" placeholder="Passwort" required/>
+                                                <Form.Control type="password" placeholder="Passwort" required />
                                             </Form.Group>
                                             <Row className="loginLinkContainer">
                                                 <Col>
                                                     <a className="loginLink" href="" title="Weiter zum Login">Hast du
-                                                        bereits einen Account?</a><br/>
+                                                        bereits einen Account?</a><br />
                                                 </Col>
                                             </Row>
                                             <Button className="primaryButton" type="submit">
