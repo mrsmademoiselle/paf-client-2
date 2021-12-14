524e064ad0d60715bd79e9b2de79be83fe4ec529
Alexander Wiederspan <alexander.wiederspan@stud.th-luebeck.de>
2021-12-14 22:11:45 +0100

    Fixen von initialem Bild

--- src/container/Register.tsx
+++ src/container/Register.tsx
@@ -5,8 +5,8 @@ import Form from 'react-bootstrap/Form';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import placeHolderImg from '../styling/images/default.png'
-import editImg from '../styling/images/buttons/edit.svg'
+import placeHolderImg from '../styling/images/default.png';
+import editImg from '../styling/images/buttons/edit.svg';
 import '../styling/css/RegisterLogin.css';
 import {Link, useNavigate} from "react-router-dom";
 import {UserAuthService} from "../services/UserAuthService";
@@ -21,6 +21,7 @@ function fileUpload(ref:any) {
  * Wenn der Benutzer über das Bild hovert, erscheint eine Drag and Drop Anzeige (siehe FX-Client)
  */
 function hoverEffect() {
+    //TODO: HOVEREFFEKT
 }
 
 /**
@@ -119,7 +120,7 @@ export default function Register() {
     }
 
     let src: any = placeHolderImg;
-    if(preview !== null || preview !== undefined){
+    if(preview !== null && preview !== undefined){
 	src = preview;
     }
 
