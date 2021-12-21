5d784f1387b1b4923bb32fccf299fb39919e4300
alexander-wiederspan <alexander.wiederspan@travekom.de>
2021-12-21 14:10:33 +0100

    #31 wording anpassen, styling auslagern, refactor

--- src/components/TopNavButtonbar.tsx
+++ src/components/TopNavButtonbar.tsx
@@ -1,6 +1,6 @@
 import MainLayout from "../layouts/MainLayout"
 import 'bootstrap/dist/css/bootstrap.min.css';
-import '../styling/css/Userprofil.css';
+import '../styling/css/Topnav.css';
 import React, {useState} from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
@@ -8,7 +8,6 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import editImg from '../styling/images/buttons/edit.svg'
-import '../styling/css/RegisterLogin.css';
 import {Link, useNavigate} from "react-router-dom";
 import {UserAuthService} from "../services/UserAuthService";
 import {TokenManager} from "../services/TokenManager";
@@ -25,20 +24,22 @@ export default function TopNavButtonbar(){
 
     return(
         <>
-            <Stack direction="horizontal">
-                <Link to="/login" onClick={deleteJwtToken}
+            {/* Buttonstack fuer Navigation */}
+            <Stack className="ButtonBarBlock" direction="horizontal" gap={5}>
+                <Link to="/userprofil"
                       className="logoutButton btn btn-outline-primary d-flex align-items-center">
                     <img src={logoutImg} className="logoutIcon" alt="Logout Symbol" title="Ausloggen"/>
-                    Log out</Link>
-                <Link to="/login" onClick={deleteJwtToken}
+                    Profil</Link>
+                <Link to="/historie"
                       className="logoutButton btn btn-outline-primary d-flex align-items-center">
                     <img src={logoutImg} className="logoutIcon" alt="Logout Symbol" title="Ausloggen"/>
-                    Log out</Link>
-                <Link to="/login" onClick={deleteJwtToken}
+                    Historie</Link>
+                <Link to="/spiel"
                       className="logoutButton btn btn-outline-primary d-flex align-items-center">
                     <img src={logoutImg} className="logoutIcon" alt="Logout Symbol" title="Ausloggen"/>
-                    Log out</Link>
+                    Spielen</Link>
             </Stack>
+            {/* Logout */}
             <Link to="/login" onClick={deleteJwtToken}
                   className="logoutButton btn btn-outline-primary d-flex align-items-center">
                 <img src={logoutImg} className="logoutIcon" alt="Logout Symbol" title="Ausloggen"/>
