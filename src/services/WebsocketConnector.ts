91fe21877678b4e8ad22f24924ccb065921ea9b3
purrito <franziska.loof@stud.th-luebeck.de>
2021-12-28 00:25:05 +0100

    Rohentwurf WebsocketConnector

---
+++ src/services/WebsocketConnector.ts
@@ -0,0 +1,28 @@
+export class WebsocketConnector {
+
+    private ws?: WebSocket;
+
+    connect(): void {
+        this.ws = new WebSocket("ws://127.0.0.1:8080");
+        this.ws.onopen = this.onOpen;
+        this.ws.onmessage = this.onMessage;
+        this.ws.onerror = this.onError;
+        this.ws.onclose = this.onClose;
+    }
+
+    onOpen(event: any): void {
+        console.log("connected");
+    }
+
+    onMessage(event: any): void {
+        console.log("message received")
+    }
+
+    onError(event: any): void {
+        console.log("error: {}", JSON.stringify(event.data));
+    }
+
+    onClose(event: any): void {
+        console.log("closed: {}", JSON.stringify(event.data));
+    }
+}
