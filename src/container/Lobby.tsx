1dce9df92c4efc55992c9ad0e8cffc433b521d66
purrito <franziska.loof@stud.th-luebeck.de>
2022-01-03 16:33:51 +0100

    Dissolve_Queue test

--- src/container/Lobby.tsx
+++ src/container/Lobby.tsx
@@ -2,7 +2,7 @@ import React, {useEffect} from "react";
 import MainLoggedInLayout from "../layouts/MainLoggedInLayout";
 import {Col, Container, Row} from "react-bootstrap";
 import ReactLoading from "react-loading"
-import {useNavigate} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
 import '../styling/css/Loadinganimation.css';
 import {GameDto} from "../entities/GameDto";
 import {addMatchDto, websocketState} from "../states/UserStates";
@@ -11,13 +11,12 @@ import {TokenManager} from "../services/TokenManager";
 
 
 export default function Lobby() {
-    let navigate = useNavigate()
-    // Dient dem cleanup der websocket subscription
-    let isMounted = true;
-
     /* States */
     const websocketConnector = useAtom(websocketState).websocketConnector;
     websocketConnector.connect();
+    let navigate = useNavigate()
+    // Dient dem cleanup der websocket subscription
+    let isMounted = true;
 
     useEffect(() => {
 
@@ -40,11 +39,17 @@ export default function Lobby() {
             let onMessage = (message: any) => parseMessage(message.data);
             websocketConnector.setOnMessage(onMessage);
             websocketConnector.sendData(JSON.stringify({"LOGIN": null, "JWT": TokenManager.getOnlyToken()}));// Dient dem cleanup der websocket subscription
+
             return () => {
                 isMounted = false
             };
         }
         , [])
+
+    let cancelQueue = () => {
+        websocketConnector.sendData(JSON.stringify({"DISSOLVE": null, "JWT": TokenManager.getOnlyToken()}));
+        navigate("/dashboard");
+    }
 
     return (
         <MainLoggedInLayout>
@@ -56,14 +61,17 @@ export default function Lobby() {
                     </Col>
                 </Row>
                 {/* Animationsblock */}
-                <Row>
-                    <Col/>
-                    <Col>
-                        <> Suche nach Spielern
-                            <ReactLoading className="reactLoader" type={"spokes"} color={"grey"} width={"100px"}/>
-                        </>
-                    </Col>
-                    <Col/>
+                <Row className="justify-content-center">
+                    <div className="h-100">
+                        <div className="h-50">Suche nach Spielern
+                            <ReactLoading className="reactLoader" type={"spokes"} color={"grey"} width={"100px"}
+                                          height={"200px"}/>
+                        </div>
+                        <div className="align-bottom">
+                            <Link to="/dashboard" onClick={cancelQueue} className="mb-auto btn btn-outline-primary">
+                                Spielsuche abbrechen
+                            </Link></div>
+                    </div>
                 </Row>
             </Container>
         </MainLoggedInLayout>
