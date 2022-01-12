ceb1fbf3b3b2378768416621a4ab32db896db5d6
alexander-wiederspan <alexander.wiederspan@travekom.de>
2022-01-12 16:17:03 +0100

    #54 aw,ck add ui, redirect, get request, ui update

--- src/container/History.tsx
+++ src/container/History.tsx
@@ -1,9 +1,70 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import MainLoggedInLayout from "../layouts/MainLoggedInLayout";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import '../styling/css/history.css';
+import {HttpConnector} from "../services/HttpConnector";
 
 export default function History() {
-    /* Bimmde hier Historiedinge maken*/
+    //States
+    const [loadedData, setloadedData] = useState<any>({
+        "losses": "",
+        "wins": "",
+        "totalGames": "",
+        "averageMoves": ""
+    });
+
+
+
+    useEffect(()=>{
+        // Abholen der Historiedaten
+        let data: Promise<Response> = HttpConnector.get("user/history");
+        data.then(
+            (d:any) => {
+                d.json().then((d:any) => setloadedData({
+                    "losses" : d.losses,
+                    "averageMoves" : d.averageMoves,
+                    "wins" : d.wins,
+                    "totalGames" : d.totalGames,
+                })
+            )}
+        ).catch(
+            (reason:any) => console.log(reason)
+        );
+    }, [])
+
+    console.log(loadedData)
     return (
-        <MainLoggedInLayout>Foobar History</MainLoggedInLayout>
+        <MainLoggedInLayout>
+            <Container>
+                <Row>
+                    Spielhistorie
+                    <hr/>
+                </Row>
+                <Row>
+                    <Col>
+                    </Col>
+                    <Col>
+                        <Row className={"textBlock"}>
+                            <Col>
+                                <p>Spiele gesamt: </p>
+                                <p>Davon gewonnen: </p>
+                                <p>Davon verloren: </p>
+                                <p>Durchschnittliche Punktzayhl: </p>
+                            </Col>
+                            <Col>
+                                <p>{loadedData.totalGames}</p>
+                                <p>{loadedData.wins}</p>
+                                <p>{loadedData.losses}</p>
+                                <p>{loadedData.averageMoves}</p>
+                            </Col>
+                        </Row>
+                    </Col>
+                    <Col>
+                    </Col>
+                </Row>
+            </Container>
+        </MainLoggedInLayout>
     )
 }
